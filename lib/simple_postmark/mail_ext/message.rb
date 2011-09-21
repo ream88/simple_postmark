e200b16711b6a1c8b1357b2cc75208bdc4daa2ab
Mario Uher <uher.mario@gmail.com>
2011-09-21 23:07:45 +0200

    Using public_send instead of send.

--- lib/simple_postmark/mail_ext/message.rb
+++ lib/simple_postmark/mail_ext/message.rb
@@ -1,5 +1,9 @@
 module Mail
   class Message
+    def html_body
+      html_part.present? ? html_part.to_postmark['Content'] : content_type && content_type.include?('text/html') ? body : nil
+    end
+    
     def tag(val = nil)
       default(:tag, val)
     end
@@ -7,24 +11,19 @@ module Mail
     def tag=(val)
       header[:tag] = val
     end
+    
+    def text_body
+      text_part.present? ? text_part.to_postmark['Content'] : body
+    end
 
     def to_postmark
       %w[attachments bcc cc from html_body reply_to subject tag text_body to].each.with_object({}) do |key, hash|        
-        hash[key.camelcase] = case (value = send(key).presence or next)
+        hash[key.camelcase] = case (value = public_send(key).presence or next)
           when AttachmentsList then value.map(&:to_postmark)
           when Array then value.join(', ')
           else value.to_s
         end
       end
-    end
-    
-    private
-    def html_body
-      html_part.present? ? html_part.to_postmark['Content'] : content_type && content_type.include?('text/html') ? body : nil
-    end
-
-    def text_body
-      text_part.present? ? text_part.to_postmark['Content'] : body
     end
   end
 end
