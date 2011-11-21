603d0505e421c9c05ebaab40d26b515770064b65
Mario Uher <uher.mario@gmail.com>
2011-11-21 11:32:57 +0100

    Added Rake task for tests.

---
+++ Rakefile
@@ -0,0 +1,8 @@
+require 'rake'
+require 'rake/testtask'
+
+Rake::TestTask.new do |t|
+  t.pattern = 'spec/*_spec.rb'
+end
+
+task(default: :test)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 $:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
+require 'minitest/autorun'
 require 'minitest/spec'
 require 'simple_postmark'
 require 'webmock/minitest'
