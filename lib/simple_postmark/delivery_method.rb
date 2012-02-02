c47de1a490a4f1218f1d95b5ac770a3ad1710c0f
Mario Uher <uher.mario@gmail.com>
2012-02-02 23:30:20 +0100

    Refactoring…

--- lib/simple_postmark/delivery_method.rb
+++ lib/simple_postmark/delivery_method.rb
@@ -1,17 +1,19 @@
 module Mail
   class SimplePostmark
     include HTTParty
-    
+
     def initialize(values)
-      self.settings = { api_key: '********-****-****-****-************' }.merge!(values)
+      self.settings = { api_key: '********-****-****-****-************' }.merge(values)
     end
-    
+
     attr_accessor :settings
     base_uri 'http://api.postmarkapp.com'
     headers 'Accept' => 'application/json', 'ContentType' => 'application/json'
-    
+
     def deliver!(mail)
-      self.class.post('/email', headers: self.class.headers.merge('X-Postmark-Server-Token' => settings[:api_key].to_s), body: mail.to_postmark.to_json)
+      api_key = { 'X-Postmark-Server-Token' => settings[:api_key].to_s }
+      
+      self.class.post('/email', headers: self.class.headers.merge(api_key), body: mail.to_postmark.to_json)
     end
   end
 end
