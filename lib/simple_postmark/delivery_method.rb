d1b14b1f69a6daadd2e3156874f4c72f60fbebf1
Mario Uher <uher.mario@gmail.com>
2013-01-01 15:46:15 +0100

    Some Ruby black magic.

--- lib/simple_postmark/delivery_method.rb
+++ lib/simple_postmark/delivery_method.rb
@@ -4,20 +4,12 @@ module Mail
 
     base_uri 'http://api.postmarkapp.com'
     headers 'Accept' => 'application/json', 'ContentType' => 'application/json'
+    delegate :post, :headers, to: self
 
     def deliver!(mail)
-      response = self.class.post('/email', headers: headers, body: get_body(mail))
+      response = post('/email', headers: headers.merge('X-Postmark-Server-Token' => settings[:api_key].to_s), body: mail.to_postmark.to_json)
       raise ::SimplePostmark::APIError.new(response) unless response.success?
       response
-    end
-
-  private
-    def headers
-      self.class.headers.merge('X-Postmark-Server-Token' => settings[:api_key].to_s)
-    end
-
-    def get_body(mail)
-      mail.to_postmark.to_json
     end
   end
 end
