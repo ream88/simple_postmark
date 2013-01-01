f692847afc014db9c0497eec279ac300b5bde2f8
Mario Uher <uher.mario@gmail.com>
2013-01-01 15:48:10 +0100

    Better readability.

--- lib/simple_postmark/delivery_method.rb
+++ lib/simple_postmark/delivery_method.rb
@@ -7,7 +7,10 @@ module Mail
     delegate :post, :headers, to: self
 
     def deliver!(mail)
-      response = post('/email', headers: headers.merge('X-Postmark-Server-Token' => settings[:api_key].to_s), body: mail.to_postmark.to_json)
+      headers = self.headers.merge('X-Postmark-Server-Token' => settings[:api_key].to_s)
+      body = mail.to_postmark.to_json
+      
+      response = post('/email', headers: headers, body: body)
       raise ::SimplePostmark::APIError.new(response) unless response.success?
       response
     end
